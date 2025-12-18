8e0d62ba1bd4de30593082d17bf0d3aa21148f0d
ains2054 <masadlawan@nemsu.edu.ph>
2025-12-18 15:48:46 +0800

    need to connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
 import 'package:chatgpt_app/landingScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load env variables
   await dotenv.load(fileName: ".env");
+
+  // 🔥 Initialize Firebase (REQUIRED)
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
       home: LandingScreen(),
     );
   }
